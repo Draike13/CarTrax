bdf4d6046add16a674fed163db375166471edc34
Trevor Biri <tmbiri91@gmail.com>
2025-08-03 17:44:39 -0500

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,10 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   namespace :api do
+    get "cars/vin/:vin", to: "cars#search_by_vin"
+
     resources :cars, only: [ :index, :show, :create, :update, :destroy ] do
       get "service_records", to: "cars#service_records"
-      get "cars/vin/:vin", to: "cars#search_by_vin"
     end
 
     resources :users, only: [ :show, :update ] do
