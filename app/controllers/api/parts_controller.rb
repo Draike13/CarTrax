a105a316c0d90bd4f6b4342a6a3f2daa3f834734
Trevor Biri <tmbiri91@gmail.com>
2025-08-04 12:17:19 -0500

    updatre parts controller. fix error in naming schemes

--- app/controllers/api/parts_controller.rb
+++ app/controllers/api/parts_controller.rb
@@ -8,9 +8,9 @@ class Api::PartsController < ApplicationController
   def create
     parts = @model.new(part_params)
     if part.save
-      render json: part, status: :created
+      render json: parts, status: :created
     else
-      render json: {errors: part.errors.full_messages}, status: :unprocessable_entity
+      render json: { errors: part.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
@@ -18,8 +18,8 @@ class Api::PartsController < ApplicationController
     part = @model.find(params[:id])
     if part.update(part_params)
       render json: part
-    else 
-      render json: { errors: part.errors.full_messages}, status: :unprocessable_entity
+    else
+      render json: { errors: part.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
@@ -34,43 +34,43 @@ class Api::PartsController < ApplicationController
   def set_part_model
     allowed_models = {
       "battery" => Parts::Battery,
-      "brake_fluid_type" => Parts::Brake_fluid_type,
-      "brake_pad" => Parts::Brake_pad,
-      "brake_rotor" => Parts::Brake_rotor,
-      "cabin_air_filter" => Parts::Cabin_air_filter,
-      "camshaft_position_sensor" => Parts::Camshaft_position_sensor,
-      "coil_pack" => Parts::Coil_pack,
-      "coolant_type" => Parts::Coolant_type,
-      "crankshaft_position_sensor" => Parts::Crankshaft_position_sensor,
-      "crankshaft_sprocket" => Parts::Crankshaft_sprocket,
-      "engine_air_filter" => Parts::Engine_air_filter,
-      "engine_oil_filter" => Parts::Engine_oil_filter,
-      "engine_oil_quantity" => Parts::Engine_oil_quanitity,
-      "engine_oil_viscosity" => Parts::Engine_oil_viscosity,
-      "head_gasket" => Parts::Head_gasket,
+      "brake fluid_type" => Parts::Brake_fluid_type,
+      "brake pad" => Parts::Brake_pad,
+      "brake rotor" => Parts::Brake_rotor,
+      "cabin air filter" => Parts::Cabin_air_filter,
+      "camshaft position sensor" => Parts::Camshaft_position_sensor,
+      "coil pack" => Parts::Coil_pack,
+      "coolant type" => Parts::Coolant_type,
+      "crankshaft position sensor" => Parts::Crankshaft_position_sensor,
+      "crankshaft sprocket" => Parts::Crankshaft_sprocket,
+      "engine air filter" => Parts::Engine_air_filter,
+      "engine oil filter" => Parts::Engine_oil_filter,
+      "engine oil quantity" => Parts::Engine_oil_quanitity,
+      "engine oil viscosity" => Parts::Engine_oil_viscosity,
+      "head gasket" => Parts::Head_gasket,
       "headlight" => Parts::Headlight,
-      "license_plate_light" => Parts::License_plate_light,
-      "maf_map_sensor" => Parts::Maf_map_sensor,
-      "serpentine_belt" => Parts::Serpentine_belt,
-      "shocks_strut" => Parts::Shocks_strut,
-      "spark_plug" => Parts::Spark_plug,
+      "license plate light" => Parts::License_plate_light,
+      "maf map sensor" => Parts::Maf_map_sensor,
+      "serpentine belt" => Parts::Serpentine_belt,
+      "shocks strut" => Parts::Shocks_strut,
+      "spark plug" => Parts::Spark_plug,
       "taillight" => Parts::Taillight,
       "thermostat" => Parts::Thermostat,
-      "throttle_position_sensor" => Parts::Throttle_position_sensor,
-      "timing_chain" => Parts::Timing_chain,
-      "timing_sprocket" => Parts::Timing_sprocket,
-      "timing_tensioner" => Parts::Timing_tensioner,
-      "tire_brand" => Parts::Tire_brand,
-      "tire_size" => Parts::Tire_size,
-      "transmission_fluid_quantity" => Parts::Transmission_fluid_quantity,
-      "transmission_fluid_type" => Parts::Transmission_fluid_type,
-      "turn_signal_light" => Parts::Turn_signal_light,
-      "valve_cover_gasket" => Parts::Valve_cover_gasket,
-      "vvt_solenoid" => Parts::Vvt_solenoid,
-      "wiper_blade_size" => Parts::Wiper_blade_size
+      "throttle position sensor" => Parts::Throttle_position_sensor,
+      "timing chain" => Parts::Timing_chain,
+      "timing sprocket" => Parts::Timing_sprocket,
+      "timing tensioner" => Parts::Timing_tensioner,
+      "tire brand" => Parts::Tire_brand,
+      "tire size" => Parts::Tire_size,
+      "transmission fluid quantity" => Parts::Transmission_fluid_quantity,
+      "transmission fluid type" => Parts::Transmission_fluid_type,
+      "turn signal light" => Parts::Turn_signal_light,
+      "valve cover gasket" => Parts::Valve_cover_gasket,
+      "vvt solenoid" => Parts::Vvt_solenoid,
+      "wiper blade size" => Parts::Wiper_blade_size
   }
     @model = allowed_models[params[:type].to_s.downcase]
-    render json: {error: "Invalid part type"}, status: :bad_request unless @model
+    render json: { error: "Invalid part type" }, status: :bad_request unless @model
   end
 
   def part_params
